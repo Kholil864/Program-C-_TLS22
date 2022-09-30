e63daa51933ab6fcf2b56988cb33da2f0008d2b8
Kholil Asjaduddin <kasjaduddin@gmail.com>
2022-09-30 19:19:03 +0700

    first commit

---
+++ TLS.cpp
@@ -0,0 +1,34 @@
+//Kasir Warteg Uddin
+#include <iostream>
+using namespace std;
+#define BURGER 15000
+#define PIZZA 40000
+#define KENTANG 10000
+#define SODA 5000
+#define MINERAL 3000
+
+int main (){
+    int m, n, total;
+    cout << "Selamat datang di Warteg Uddin\nMenu:\n 1. Burger\n 2. Pizza\n 3. Kentang Goreng\n 4. Soda\n 5. Air Mineral\n";
+    cout << "Silahkan pilih nomor menu: ";
+    cin  >> m;
+    if (m==1) {
+        m=BURGER;
+    } else if (m==2) {
+        m=PIZZA;
+    } else if (m==3) {
+        m=KENTANG;
+    } else if (m==4) {
+        m=SODA;
+    } else if (m==5) {
+        m=MINERAL;
+    } else { 
+        cout << "menu tidak tersedia";
+    }
+    cout << "Jumlah:";
+    cin  >> n;
+    total = n*m;
+    cout << "Total pembayaran: " << total;
+
+    return 0;
+}
